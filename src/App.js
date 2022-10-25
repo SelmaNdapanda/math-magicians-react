172c45ddeaf6aca2bb971ce1f375da1b9d245d4f
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-10-25 15:05:47 +0200

    Fix linters errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator/>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,71 @@
 import React from 'react';
 import './App.css';
 
-class Calculator extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="container">
-          <input type="text" className="display" id="display" value={0} />
-          <div className="keypads">
-            <button className="left-btns" id="clear">AC</button>
-            <button className="left-btns" name="+/-">+/-</button>
-            <button className="left-btns" name="%">%</button>
-            <button className="right-btns" name="/">&divide;</button>
-            <button className="left-btns" name="7">7</button>
-            <button className="left-btns" name="8">8</button>
-            <button className="left-btns" name="9">9</button>
-            <button className="right-btns" name="*">&times;</button>
-            <button className="left-btns" name="4">4</button>
-            <button className="left-btns" name="5">5</button>
-            <button className="left-btns" name="6">6</button>
-            <button className="right-btns" name="-">&ndash;</button>
-            <button className="left-btns" name="1">1</button>
-            <button className="left-btns" name="2">2</button>
-            <button className="left-btns" name="3">3</button>
-            <button className="right-btns" name="+">+</button>
-            <button className=" left-btns zero" name="0">0</button>
-            <button className="left-btns" name=".">.</button>
-            <button className="right-btns" name="=">=</button>
-          </div>
-        </div>
-      </> 
-    )
-  }
-}
+const Calculator = () => (
+  <>
+    <div className="container">
+      <input type="text" className="display" id="display" value={0} />
+      <div className="keypads">
+        <button type="button" className="left-btns" id="clear">
+          AC
+        </button>
+        <button type="button" className="left-btns" name="+/-">
+          +/-
+        </button>
+        <button type="button" className="left-btns" name="%">
+          %
+        </button>
+        <button type="button" className="right-btns" name="/">
+          &divide;
+        </button>
+        <button type="button" className="left-btns" name="7">
+          7
+        </button>
+        <button type="button" className="left-btns" name="8">
+          8
+        </button>
+        <button type="button" className="left-btns" name="9">
+          9
+        </button>
+        <button type="button" className="right-btns" name="*">
+          &times;
+        </button>
+        <button type="button" className="left-btns" name="4">
+          4
+        </button>
+        <button type="button" className="left-btns" name="5">
+          5
+        </button>
+        <button type="button" className="left-btns" name="6">
+          6
+        </button>
+        <button type="button" className="right-btns" name="-">
+          &ndash;
+        </button>
+        <button type="button" className="left-btns" name="1">
+          1
+        </button>
+        <button type="button" className="left-btns" name="2">
+          2
+        </button>
+        <button type="button" className="left-btns" name="3">
+          3
+        </button>
+        <button type="button" className="right-btns" name="+">
+          +
+        </button>
+        <button type="button" className=" left-btns zero" name="0">
+          0
+        </button>
+        <button type="button" className="left-btns" name=".">
+          .
+        </button>
+        <button type="button" className="right-btns" name="=">
+          =
+        </button>
+      </div>
+    </div>
+  </>
+);
+
 export default Calculator;
