b045947e06f119f67348a24eb0d2b99d1b93c10f
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-10-25 21:24:00 +0200

    Fix linters errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-const App = () => (
-  <div className="App">
-    <Calculator />
-  </div>
-);
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
 
 export default App;
