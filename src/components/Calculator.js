18349b354ae903d1e0d47ee7d63daacbf780a865
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-02 16:32:25 +0200

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import './App.css';
-import calculate from '../logic/calculate';
+import React, { useState } from "react";
+import "./App.css";
+import calculate from "../logic/calculate";
 
 const Calculator = () => {
   const [calc, setCalc] = useState({ total: 0, operation: null, next: null });
@@ -11,34 +11,183 @@ const Calculator = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="display" id="display">
-          {calc.total}
-          {' '}
-          {calc.operation}
-          {' '}
-          {calc.next}
-        </div>
-        <div className="keypads">
-          <button type="button" onClick={clickHandler} className="left-btns" id="clear">AC</button>
-          <button type="button" onClick={clickHandler} className="left-btns plusminus" name="+/-">+/-</button>
-          <button type="button" onClick={clickHandler} className="left-btns mod" name="%"> % </button>
-          <button type="button" onClick={clickHandler} className="right-btns div" name="/"> &divide; </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="7"> 7 </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="8"> 8 </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="9"> 9 </button>
-          <button type="button" onClick={clickHandler} className="right-btns times" name="*"> x </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="4"> 4 </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="5"> 5 </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="6"> 6 </button>
-          <button type="button" onClick={clickHandler} className="right-btns minus" name="-"> - </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="1"> 1 </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="2"> 2 </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="3"> 3 </button>
-          <button type="button" onClick={clickHandler} className="right-btns plus" name="+"> + </button>
-          <button type="button" onClick={clickHandler} className=" left-btns zero" name="0"> 0 </button>
-          <button type="button" onClick={clickHandler} className="left-btns" name="."> . </button>
-          <button type="button" onClick={clickHandler} className="right-btns" name="="> = </button>
+      <div className="calculator-page">
+        <h2 className="calc-title">Let's do some Math!</h2>
+        <div className="container">
+          <div className="display" id="display">
+            {calc.total} {calc.operation} {calc.next}
+          </div>
+          <div className="keypads">
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              id="clear"
+            >
+              AC
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns plusminus"
+              name="+/-"
+            >
+              +/-
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns mod"
+              name="%"
+            >
+              {" "}
+              %{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="right-btns div"
+              name="/"
+            >
+              {" "}
+              &divide;{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="7"
+            >
+              {" "}
+              7{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="8"
+            >
+              {" "}
+              8{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="9"
+            >
+              {" "}
+              9{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="right-btns times"
+              name="*"
+            >
+              {" "}
+              x{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="4"
+            >
+              {" "}
+              4{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="5"
+            >
+              {" "}
+              5{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="6"
+            >
+              {" "}
+              6{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="right-btns minus"
+              name="-"
+            >
+              {" "}
+              -{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="1"
+            >
+              {" "}
+              1{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="2"
+            >
+              {" "}
+              2{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="3"
+            >
+              {" "}
+              3{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="right-btns plus"
+              name="+"
+            >
+              {" "}
+              +{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className=" left-btns zero"
+              name="0"
+            >
+              {" "}
+              0{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="left-btns"
+              name="."
+            >
+              {" "}
+              .{" "}
+            </button>
+            <button
+              type="button"
+              onClick={clickHandler}
+              className="right-btns"
+              name="="
+            >
+              {" "}
+              ={" "}
+            </button>
+          </div>
         </div>
       </div>
     </>
