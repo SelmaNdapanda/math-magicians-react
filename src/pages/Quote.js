ec37719fd79c874b1204046a4aabf6a79700ea9d
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-02 16:29:45 +0200

    Create Pages

---
+++ src/pages/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../components/App.css';
+
+const Quote = () => {
+    return (
+        <div className='quote'>
+            <p> "Pure mathematics is, in its way, the poetry of logical ideas."</p>
+            <p className='name'>~ Albert Einstein</p>
+        </div>
+    );
+};
+
+export default Quote;
