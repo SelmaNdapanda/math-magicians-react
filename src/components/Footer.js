b228e691f484085323f2ba5106380d9045241984
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-11-02 16:31:13 +0200

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className='copyright'>
+                <h4>Designed by Selma-Hamutenya</h4>
+                <p>Copyright@2022</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
